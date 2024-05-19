e586368fd4bbca13cdfd14b3beb965d64a793894
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-05-19 16:49:06 -0600

    terminamos hook de usePostReservacion

---
+++ src/shared/hooks/usePostReservacion.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+//import { useNavigate } from "react-router-dom";
+import { postReservacion as postReservacionRequest } from "../../services/api";
+import toast from "react-hot-toast";
+
+export const usePostReservacion = () => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    //const navigate = useNavigate();
+
+    const postReservacion = async (habitacionId, fechaInicio, fechaFin, huespedes ) => {
+        setIsLoading(true);
+
+        const response = await postReservacionRequest({
+            habitacionId,
+            fechaInicio,
+            fechaFin,
+            huespedes
+        })
+
+        setIsLoading(false)
+        if(response.error){
+            return toast.error(
+                response.e?.response?.data || 'Ocurrió un error al crear la reservación'
+            )
+        }
+    }
+
+    return {
+        postReservacion,
+        isLoading
+    }
+
+    
+}
+
