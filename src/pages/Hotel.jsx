6c3791b80e3492aa2fbe63e627e17f27b0f804b7
Jose Tejaxun <jtejaxun-2022229@kinal.edu.gt>
2024-05-19 23:02:44 -0600

    se trabaja componente hoteles

---
+++ src/pages/Hotel.jsx
@@ -0,0 +1,51 @@
+import axios from "axios"
+import HotelCard from "../components/HotelCard"
+import { useContext, useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
+
+const Hotel = () => {
+
+    const { search } = useLocation()
+    const [posts, setPosts] = useState([])
+    const [noResults, setNoResults] = useState(false)
+    const [loader, setLoader] = useState(false)
+
+    const fetchPosts = async () => {
+
+        setLoader(true)
+
+        try {
+
+
+        }
+        catch (err) {
+
+        }
+    }
+
+    useEffect(() => {
+
+        fetchPosts()
+
+    }, [search])
+
+
+
+    return (
+        <>
+
+            <div className="px-8 md:px-[200px] min-h-[80vh]">
+                {loader ? <div className="h-[40vh] flex justify-center items-center"><Loader /></div> : !noResults ?
+                    posts.map((post) => (
+                        <>
+                            <Link to={user ? `/posts/post/${post._id}` : "/login"}>
+                                <HomePosts key={post._id} post={post} />
+                            </Link>
+                        </>
+                    )) : <h3 className="text-center font-bold mt-16">No hay Blogs disponibles</h3>}
+            </div>
+        </>
+    )
+}
+
+export default Home
